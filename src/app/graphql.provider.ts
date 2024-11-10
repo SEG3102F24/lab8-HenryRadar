a4a4369d7e66c30e3095258b3ce560184067637f
Henry Radar <henry.radar@gmail.com>
2024-11-10 17:05:02 -0500

    Set up initial project to work with graphql

--- src/app/graphql.provider.ts
+++ src/app/graphql.provider.ts
@@ -1,10 +1,10 @@
 import { Apollo, APOLLO_OPTIONS } from 'apollo-angular';
 import { HttpLink } from 'apollo-angular/http';
 import { ApplicationConfig, inject } from '@angular/core';
-import { ApolloClientOptions, InMemoryCache } from '@apollo/client/core';
+import { InMemoryCache } from '@apollo/client/core';
 
 const uri = 'http://localhost:9000/graphql'; // <-- add the URL of the GraphQL server here
-export function apolloOptionsFactory(): ApolloClientOptions<any> {
+export function apolloOptionsFactory() {
   const httpLink = inject(HttpLink);
   return {
     link: httpLink.create({ uri }),
